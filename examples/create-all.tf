99dfc0d7db0de9095fb775840a81850b1aea61a7
nsantiago <nsantiago@yondu.com>
2019-06-11 15:57:10 +0800

    add example

---
+++ examples/create-all.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = "us-east-1"
+  shared_credentials_file = "~/.aws/credentials"
+}
+
+resource "aws_key_pair" "key-pair" {
+  key_name   = "simple-key-pair"
+  public_key = "${file("${path.cwd}/keypair")}"
+}
+
+
+module "project-vpc" {
+  source = "./modules/vpc"
+}
+
+module "security-group-public" {
+  source = "./modules/security-groups"
+
+  name = "simple-project-default"
+  description = "allow port 80"
+  vpc-id = "${module.project-vpc.vpc-id}"
+  ingress-ports = ["80", "443"]
+  allowed-cidr-block = ["0.0.0.0/0"]
+}
+
+module "security-group-instance" {
+  source = "./modules/security-groups"
+
+  name = "simple-project-sg"
+  description = "allow port 80"
+  vpc-id = "${module.project-vpc.vpc-id}"
+  ingress-ports = ["80"]
+  security-group-ids = ["${module.security-group-public.id}"]
+}
+
+module "instances" {
+  source = "./modules/ec2"
+
+  desired-instance = 3
+  project = "simple-project"
+  subnet-ids = "${module.project-vpc.private-subnet-ids}"
+  security-group-ids = ["${module.security-group-instance.id}"]
+  vpc-id = "${module.project-vpc.vpc-id}"
+  key-pair = "${aws_key_pair.key-pair.key_name}"
+  user-data = "${file("${path.cwd}/modules/install_nginx/install")}"
+}
+
+module "elb" {
+  source = "./modules/elb"
+
+  name = "simple-project-elb"
+  instance-ids = "${module.instances.ids}"
+  subnets = "${module.project-vpc.public-subnet-ids}"
+  security-groups = ["${module.security-group-public.id}"]
+  vpc-id = "${module.project-vpc.vpc-id}"
+  certificate-arn = "${var.certificate-arn}"
+}
+
+
