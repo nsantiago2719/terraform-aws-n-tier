f6855588e1d00a4cb460b84f1f27f8c2189ee237
nsantiago <nsantiago@yondu.com>
2019-06-16 12:32:53 +0800

    update terraform block on main.tfs

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,7 +2,11 @@
 # Terraform version should be used by this template
 #---------------------------------------------------------------------------------------
 terraform {
-  required_version = "0.12.1"
+  required_version = ">= 0.12.1,<= 0.12.5"
+
+  required_providers {
+    aws = "~> 2.15.0"
+  }
 }
 
 resource "aws_vpc" "vpc" {
